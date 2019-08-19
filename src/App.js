15d9838cb4c3975b2af9a219b0062059c6ea8754
wickieonya <wickieonya@gmail.com>
2019-08-19 06:27:03 +0300

    react state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const my_list = [
+    {
+        title: 'Road to react- 2019',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 103,
+        points: 400,
+        object_id: 0
+    },
+    {
+        title: 'React devs in Kenya',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 103,
+        points: 400,
+        object_id: 2
+    },
+    {
+        title: 'React makes me happy.',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 103,
+        points: 400,
+        object_id: 3
+    },
+    {
+        title: 'Just another indication that this is the thing.',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 103,
+        points: 400,
+        object_id: 4
+    }
+];
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            my_list,
+        };
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                { this.state.my_list.map( (item) => {
+                    return (
+                        <div key={item.object_id} className='jumbotron'>
+                            <span> <a href={ item.url }> { item.title } </a></span>&nbsp;
+                            <span>Author: { item.author }</span>&nbsp;
+                            <span>Commnents: { item.num_comments }</span>&nbsp;
+                            <span>Points: { item.points }</span>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
